e2c62fa594f7e4f921f9a050f66c7b7be7f5fda1
Matyika György <matyikaheni@gmail.com>
2026-05-16 16:42:32 +0200

    Ladder and Oregen preparations

--- Assets/DungeonScripts/TilemapVisualizer.cs
+++ Assets/DungeonScripts/TilemapVisualizer.cs
@@ -1,25 +1,35 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using Random = UnityEngine.Random;
 
 public class TilemapVisualizer : MonoBehaviour
 {
     [SerializeField]
-    private Tilemap floorTilemap, wallTilemap;
+    private Tilemap floorTilemap, wallTilemap, itemTilemap, ladderTilemap;
     [SerializeField]
-    private TileBase floorTile, wallTile;
+    private TileBase floorTile, wallTile, ladderTile, oresTile;
 
-    public void PaintFloorTiles(IEnumerable floorPositions)
+
+    public void PaintFloorTiles(HashSet<Vector2Int> floorPositions)
     {
         PaintTiles(floorPositions, floorTilemap, floorTile);
     }
 
-    private void PaintTiles(IEnumerable positions, Tilemap tilemap, TileBase tile)
+    private void PaintTiles(HashSet<Vector2Int> positions, Tilemap tilemap, TileBase tile)
     {
         foreach (Vector2Int position in positions)
         {
             PaintSingleTile(tilemap, tile, position);
+        }
+        for (int i = 0; i < positions.Count / 300; i++)
+        {
+            var ladderLocation = positions.ElementAt(Random.RandomRange(0, positions.Count));
+            Debug.Log(ladderLocation);
+            PaintSingleLadder(ladderLocation, ladderTilemap, ladderTile);
         }
     }
 
@@ -31,6 +41,7 @@ public class TilemapVisualizer : MonoBehaviour
 
     public void Clear()
     {
+        ladderTilemap.ClearAllTiles();
         floorTilemap.ClearAllTiles();
         wallTilemap.ClearAllTiles();
     }
@@ -38,5 +49,11 @@ public class TilemapVisualizer : MonoBehaviour
     internal void PaintSingleBasicWall(Vector2Int position)
     {
         PaintSingleTile(wallTilemap, wallTile, position);
+    }
+
+    static internal void PaintSingleLadder(Vector2Int position, Tilemap tilemap, TileBase tile)
+    {
+        var tilePosition = tilemap.WorldToCell((Vector3Int)position);
+        tilemap.SetTile(tilePosition, tile);
     }
 }
