cbf4b64ff73952844595d545544c058bd266ddb5
Matyika György <matyikaheni@gmail.com>
2026-05-29 12:25:27 +0200

    Fade In and Out animations for climbing the ladder

---
+++ Assets/ScreenFadeTransition.cs
@@ -0,0 +1,36 @@
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class ScreenFadeTransition : MonoBehaviour
+{
+    public static ScreenFadeTransition Instance;
+    [SerializeField] CanvasGroup canvasGroup;
+    [SerializeField] float fadeDuration;
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    async Task Fade(float targetTransparency)
+    {
+        float start = canvasGroup.alpha, t = 0;
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            canvasGroup.alpha = Mathf.Lerp(start, targetTransparency, t / fadeDuration);
+            await Task.Yield();
+        }
+        canvasGroup.alpha = targetTransparency;
+    }
+
+    public async Task FadeOut()
+    {
+        await Fade(1);
+    }
+
+    public async Task FadeIn()
+    {
+        await Fade(0);
+    }
+}
