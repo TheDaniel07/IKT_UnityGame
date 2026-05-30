8b49ebeb80b9e5bac15648a251b5185fbcb39efe
Matyika György <matyikaheni@gmail.com>
2026-05-30 15:57:38 +0200

    Torch placement added, +animations for it

---
+++ Assets/AttackScripts/TorchPlacement.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TorchPlacement : MonoBehaviour
+{
+    public GameObject Torch;
+    public Transform parent;
+    public Rigidbody2D player;
+
+    public void PlaceTorch(InputAction.CallbackContext context)
+    {
+        Vector3 playerPosition = player.transform.position;
+        Instantiate(Torch, playerPosition, Quaternion.identity, parent);
+    }
+
+}
