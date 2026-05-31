2c50f1d595d77a0345661bbab4b5559317747e0b
Matyika György <matyikaheni@gmail.com>
2026-05-31 11:10:18 +0200

    Inventory Scene merged, Enemy Generator Added

--- Assets/DungeonScripts/EnemyGenerator.cs
+++ Assets/DungeonScripts/EnemyGenerator.cs
@@ -1,16 +1,30 @@
+using System.Collections.Generic;
+using System.Linq;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class EnemyGenerator : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Transform parent;
+    [SerializeField] private OreGenerator oregen;
+    [SerializeField] GameObject enemy;
+    public void SpawnEnemies(HashSet<Vector2Int> positions)
     {
         
+
+        for (int i = 0; i < positions.Count / 300; i++)
+        {
+            Vector2Int randomPos = positions.ElementAt(UnityEngine.Random.Range(0, positions.Count));
+            Vector3 enemyPos = new Vector3(randomPos.x, randomPos.y)+ new Vector3(0.5f, 0.5f);
+            Instantiate(enemy, enemyPos, Quaternion.identity, parent);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void KillAllEnemies()
     {
-        
+        foreach(Transform child in parent)
+        {
+            Destroy(child.gameObject);
+        }
     }
 }
