4ded39ea21291fa1814e55b23d7d568d7ffee4f4
Matyika György <matyikaheni@gmail.com>
2026-05-31 11:41:32 +0200

    Enemy Changes

--- Assets/AIChase.cs
+++ Assets/AIChase.cs
@@ -30,6 +30,9 @@ public class AIChase : MonoBehaviour
 
         if (distance < distanceBetween)
         {
+            Vector3 LookDirection = player.transform.position - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, angle-90);
             transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
         }
 
